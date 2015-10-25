66e505c0bb3cb845144e2f2cb341e8ed3a391fcc
Frank Shen <zhshen@gmail.com>
2015-10-25 22:36:56 +0800

    加入一本哈利波特測試案例紅燈

--- ShoppingCart.Tests/CartTest.cs
+++ ShoppingCart.Tests/CartTest.cs
@@ -1,14 +1,51 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ShoppingCart;
 
 namespace ShoppingCart.Tests
 {
     [TestClass]
     public class CartTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static IList<Book> _books;
+
+        [ClassInitialize()]
+        public static void CartTestInitialize(TestContext TestContext)
         {
+            _books = new List<Book>();
+            _books.Add(new Book { Id = 1, Name = "Harry Potter and the Philosopher's Stone", SellPrice = 100 });
+            _books.Add(new Book { Id = 2, Name = "Harry Potter and the Chamber of Secrets", SellPrice = 100 });
+            _books.Add(new Book { Id = 3, Name = "Harry Potter and the Prisoner of Azkaban", SellPrice = 100 });
+            _books.Add(new Book { Id = 4, Name = "Harry Potter and the Goblet of Fire", SellPrice = 100 });
+            _books.Add(new Book { Id = 5, Name = "Harry Potter and the Order of the Phoenix", SellPrice = 100 });
+        }
+
+        [ClassCleanup()]
+        public static void CartTestCleanup()
+        {
+            _books = null;
+        }
+
+        /// <summary>
+        /// 加入一本哈利波特書籍至購物車中並結帳，預期結帳金額應等於其售價 100
+        /// </summary>
+        [TestMethod]
+        public void Test_add_1_book_to_cart_then_checkout_total_should_be_equal_sell_price()
+        {
+            // Arrange
+            var book = new Book { Id = 1, Name = "Harry Potter and the Philosopher's Stone", SellPrice = 100 };
+            var expected = book.SellPrice;
+
+            var target = new Cart();
+            target.Add(book);
+            target.Checkout();
+
+            // Act
+            var actual = target.Total;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
---
+++ ShoppingCart/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart
+{
+    public class Cart
+    {
+        public int Total { get; private set; }
+
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Checkout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
