ef3c3964ac2032b29b1d093535b26398a606634c
Frank Shen <zhshen@gmail.com>
2015-10-26 22:40:08 +0800

    第五測試案例失敗

--- ShoppingCart.Tests/CartTest.cs
+++ ShoppingCart.Tests/CartTest.cs
@@ -122,5 +122,34 @@ namespace ShoppingCart.Tests
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        /// <summary>
+        /// 加入五本不同書籍至購物車中並結帳，預期結帳金額應等於其售價 100 * 5 * 0.75
+        /// </summary>
+        [TestMethod]
+        public void Test_add_5_different_books_to_cart_then_checkout_total_should_be_equal_sell_price_25_percent_save_off()
+        {
+            // Arrange
+            var book_1 = new Book { Id = 1, Name = "Harry Potter and the Philosopher's Stone", SellPrice = 100 };
+            var book_2 = new Book { Id = 2, Name = "Harry Potter and the Chamber of Secrets", SellPrice = 100 };
+            var book_3 = new Book { Id = 3, Name = "Harry Potter and the Prisoner of Azkaban", SellPrice = 100 };
+            var book_4 = new Book { Id = 4, Name = "Harry Potter and the Goblet of Fire", SellPrice = 100 };
+            var book_5 = new Book { Id = 5, Name = "Harry Potter and the Order of the Phoenix", SellPrice = 100 };
+            var expected = (book_1.SellPrice + book_2.SellPrice + book_3.SellPrice + book_4.SellPrice + book_5.SellPrice) * 0.75;
+
+            var target = new Cart();
+            target.Add(book_1);
+            target.Add(book_2);
+            target.Add(book_3);
+            target.Add(book_4);
+            target.Add(book_5);
+            target.Checkout();
+
+            // Act
+            var actual = target.Total;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
